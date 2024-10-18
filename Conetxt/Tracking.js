42f20748b956b5bdea54a5f15a847de325f1dfcf
Nischal Shrestha <nischalstha08@gmail.com>
2024-10-18 15:52:31 +0545

    Second Commit

--- Conetxt/Tracking.js
+++ Conetxt/Tracking.js
@@ -3,7 +3,7 @@ import Web3Modal from "web3modal";
 import {ethers} from "ethers";
 
 //INTERNAL iMPORT
-import tracking from "../Conetxt/Tracking.json";
+import tracking from "./Tracking.json";
 const ContractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 const ContractABI = tracking.abi;
 
